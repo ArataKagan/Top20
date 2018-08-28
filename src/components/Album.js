2f603c4808d47b9fa9a1dc957844ea440b8b6fa8
Arata Kagan <aratakagan@gmail.com>
2018-08-27 17:48:13 -0700

    checkpoint-2761-player-bar-range-input: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -34,6 +34,25 @@ class Album extends Component {
     this.setState({ isPlaying: false });
   }
 
+  componentDidMount(){
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount(){
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
   setSong(song) {
     this.audioElement.src = song.audioSrc;
     this.setState({ currentSong: song });
@@ -64,6 +83,12 @@ class Album extends Component {
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
+  }
+
+  handleTimeChange(e){
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
   }
 
   onMouseEnter(index){
@@ -126,6 +151,7 @@ class Album extends Component {
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
         />
       </section>
     );
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -20,10 +20,12 @@ class PlayerBar extends Component {
         <input
           type='range'
           className='seek-bar'
-          value={(this.props.currentTime / this.props.duration) || 0} 
+          value={(this.props.currentTime / this.props.duration) || 0}
           max='1'
           min='0'
-          step='0.01'/>
+          step='0.01'
+          onChange={this.props.handleTimeChange}
+          />
         <div className='total-time'>{this.props.duration}</div>
       </section>
       <section id='volume-control'>
