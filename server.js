a67d082c1a903dcaf835ca8cbcd837e5578aac7f
Arata Kagan <aratakagan@gmail.com>
2019-03-18 22:44:52 -0700

    Debugging deploying to heroku

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/", (req, res, next) => {
+    res.send("Welcome to TV list app");
+})
+
+
 // app.get('/api/hello', (req, res) => {
 //     res.send({ express: 'Hello from Express'});
 // });
@@ -17,5 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //         `I received your POST request. This is what you sent me: ${req.body.post}`,
 //     );
 // });
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
