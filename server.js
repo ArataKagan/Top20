f3f8632a4d4413788673fed02cb7ffd0cae19407
Arata Kagan <aratakagan@gmail.com>
2019-03-18 23:22:21 -0700

    Added heroku-postbuild

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json());
 
 
 
@@ -30,4 +27,5 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
