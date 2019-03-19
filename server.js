a1331d2819be8fbb5957a4ae62b4c2fb911bf452
Arata Kagan <aratakagan@gmail.com>
2019-03-18 23:06:31 -0700

    debugging deployment

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", (req, res, next) => {
-    res.send("Welcome to TV list app");
-})
+
 
 
 // app.get('/api/hello', (req, res) => {
