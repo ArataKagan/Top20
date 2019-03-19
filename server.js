01527c591173ac0a02b6dff7fac0a3a6be0a1464
Arata Kagan <aratakagan@gmail.com>
2019-03-18 22:50:41 -0700

    debugging heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
