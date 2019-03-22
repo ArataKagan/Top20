3d3007ebb76938c305a2afbe2b33c2e82a65b1a3
Arata Kagan <aratakagan@gmail.com>
2019-03-21 23:57:25 -0700

    Changed background image and logo font

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-/* Need to setup router to assign a path for each component*/
-/*import Route and Link from react-router-dom package */
 import { Route, Link } from 'react-router-dom';
 import './App.css';
-import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
@@ -13,17 +10,14 @@ class App extends Component {
       <div className="App" >
         <header>
           <nav className = 'navbar navbar-expand-lg navbar-dark bg-dark'>
-            <a className="navbar-brand" href='#'>Top 20</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <a className="navbar-brand" href='/'>Top 20</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className= 'navbar-nav mr-auto'>
                 <li className="nav-item">
-                  <Link to='/' className="nav-link">Landing</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/library' className="nav-link">Library</Link>
+                  <Link to='/' className="nav-link">Library</Link>
                 </li>
               </ul>
             </div>
@@ -31,8 +25,7 @@ class App extends Component {
           
         </header>
         <main>
-          <Route exact path='/' component={Landing} />
-          <Route path='/library' component={Library} />
+          <Route exact path='/' component={Library} />
           <Route path='/album/:id' component={Album} />
         </main>
       </div>
